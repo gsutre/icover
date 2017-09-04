358bcb62e7a15c8174e2f746d4dc489a9f5405ac
Grégoire Sutre <gregoire.sutre@gmail.com>
2017-09-04 16:49:41 +0200

    Adapt the all_benchmarks.sh script to the new benchmarks.sh script.

--- benchmarks/all_benchmarks.sh
+++ benchmarks/all_benchmarks.sh
@@ -4,6 +4,9 @@ TOOLS="icover qcover petrinizer mist-backward bfc"
 
 for tool in $TOOLS
 do
+    echo "Benchmarking tool \`$tool'"
+    if [ "$tool" = "icover" ]; then
+	tool=""
+    fi
     ./benchmark.sh $tool
 done
-
